b934aaffc1db43aad089900083dca2bd0027c6ee
chaflame <chaflame@chaflameui-iMac.local>
2021-06-28 21:50:58 +0900

    add textfield

--- src/ButtonPanel.java
+++ src/ButtonPanel.java
@@ -9,16 +9,21 @@ class ButtonPanel extends JPanel {
         JButton convertButton = new JButton("Convert");
         JButton removeButton = new JButton("Remove");
 
-        addButton.setBackground(new Color(177, 175, 168));
-        convertButton.setBackground(new Color(177, 175, 168));
-        removeButton.setBackground(new Color(177, 175, 168));
+
+        Color color = new Color(0, 108, 227);
+
         addButton.setBounds(0, 10, 100, 50);
         convertButton.setBounds(0, 70, 100, 50);
         removeButton.setBounds(0, 130, 100, 50);
+        addButton.setBackground(Color.BLUE);
+        convertButton.setBackground(new Color(123, 128, 133));
+        removeButton.setBackground(new Color(123, 128, 133));
 
 
         add(addButton);
         add(convertButton);
         add(removeButton);
+
+
     }
 }
--- src/TextPanel.java
+++ src/TextPanel.java
@@ -14,9 +14,13 @@ class TextPanel extends JPanel {
         JTextField convertTextField = new JTextField();
         JTextField removeTextField = new JTextField();
 
-        addLabel.setBounds(0, 50, 80, 50);
-        convertLabel.setBounds(0, 150, 80, 50);
-        removeLabel.setBounds(0, 250, 80, 50);
+        addLabel.setBounds(0, 50, 50, 30);
+        convertLabel.setBounds(0, 150, 50, 30);
+        removeLabel.setBounds(0, 250, 50, 30);
+        addTextField.setBounds(80, 50, 100, 30);
+        originalTextField.setBounds(80, 150, 100, 30);
+        convertTextField.setBounds(200, 150, 100, 30);
+        removeTextField.setBounds(80, 250, 100, 30);
 
         add(addLabel);
         add(convertLabel);
